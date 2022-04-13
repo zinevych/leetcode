9250f223b0667f47cdfd0ebc1ecb1586c52dfa90
Aleks Zinevych <aleksanderzinewicz@gmail.com>
2022-04-13 23:24:52 +0300

    solution

---
+++ 24.Swap Nodes in Pairs/index.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var swapPairs = function(head) {
+  if(!head || !head.next)
+      return head;
+  const temp = head;
+  head = head.next;
+  temp.next = swapPairs(temp.next.next)
+  head.next = temp;
+  
+  return head;
+};
