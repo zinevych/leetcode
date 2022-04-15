4d7af76f23ca4de3ecb4881d426bca0b2039e1c3
Aleks Zinevych <aleksanderzinewicz@gmail.com>
2022-04-15 23:35:19 +0300

    solution

---
+++ 70.Climb Stairs/index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ var climbStairs = function(n) {    
+  let prev = 0;
+  let cur = 1;
+  let temp;
+  
+  for (let i = 0; i < n; i++) {
+      temp = prev;
+      prev = cur;
+      cur += temp; 
+      console.log('---', cur)
+  }
+  return cur;
+};
