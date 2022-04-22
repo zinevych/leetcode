2e8c4ee6b635373213e04a6fa9c606765284f797
Aleks Zinevych <aleksanderzinewicz@gmail.com>
2022-04-22 23:29:22 +0300

    sollution

---
+++ 189.Rotate Array/index.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+ var rotate = function(nums, k) {
+  k = k % nums.length;
+  const temp = nums.slice(-k).concat(nums.slice(0, nums.length - k))
+  
+  nums.forEach((item, index) => {
+      nums[index] = temp[index]
+  })
+};
